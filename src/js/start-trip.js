80d8d3fa62457e7db8fd5b841b379193c1b84c35
DANIEL REVO <DANIELREVO08@GMAIL.COM>
2016-04-23 10:09:26 -0400

    calendar working so users can pick start date. Using that start date to successfully print to the screen every city that hosts a home game on that evening using the seatgeek API.

--- src/js/start-trip.js
+++ src/js/start-trip.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { ajax } from 'jquery';
 
 import DatePicker from 'material-ui/lib/date-picker/date-picker';
 import ReactDatePicker from 'react-date-picker';
@@ -9,13 +10,52 @@ import ReactDatePicker from 'react-date-picker';
 
 export default class StartTrip extends Component{
 
+	constructor(...args){
+		super(...args)
+
+		this.state = {
+
+			citiesWithGames: []
+
+		}
+
+	}
+
+	dateChangeHandler(dateString){
+
+		let URL = `https://api.seatgeek.com/2/events?datetime_local.gte=${dateString}&datetime_local.lte=${dateString}T23:59:01&type=mlb&per_page=15`;
+		let citiesWithGames = [];
+
+		ajax(URL).then( data => {
+
+			data.events.map(event => {
+
+			citiesWithGames.push(event.venue.city);
+
+			});
+
+			this.setState({citiesWithGames});
+
+		});
+
+	}
+
 
 	render(){
+
+		let { citiesWithGames } = this.state;
 
 		return(
 
 			<div>
-				<ReactDatePicker />
+				<ReactDatePicker onChange={::this.dateChangeHandler}/>
+
+				<ul>
+
+					{citiesWithGames.map(city => <li key={city}>{city}</li>)}
+
+				</ul>
+
 			</div>
 			);
 
